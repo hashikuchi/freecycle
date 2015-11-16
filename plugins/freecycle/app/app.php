b101de94f7e9135b5898563070ecfac810fd6e4d
beak <k.hashikuchi@gmail.com>
2015-11-16 22:13:26 +1300

    Add functions for app logout feature

--- plugins/freecycle/app/app.php
+++ plugins/freecycle/app/app.php
@@ -2,8 +2,20 @@
 
 /**
  * freecycle/app/app.php
- * 
+ *
  * Functions called from the smartphone app.
  */
+
+add_action('wp_ajax_get_nonce_from_app', 'get_nonce_from_app');
+add_action('wp_ajax_nopriv_get_nonce_from_app', 'get_nonce_from_app');
+
+/**
+ * 認証用のnonceを取得します。
+ */
+function get_nonce_from_app(){
+	$action = isset($_REQUEST["nonce_action"])?$_REQUEST["nonce_action"]:"";
+	echo wp_create_nonce($action);
+	die;
+}
 
 ?>
