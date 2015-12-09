23a5f657115af4abaee5d1bda473279259738a50
yugogura <jigenbakdaaan@gmail.com>
2015-12-10 01:32:20 +0900

    can use as page low quality have to make beautiful...

--- themes/freecycle/admin/views/admin_window.php
+++ themes/freecycle/admin/views/admin_window.php
@@ -4,8 +4,8 @@
 			</div>
             <div class="wrap">
 				<div class="inner">
-					<div class="box"><a href="https://github.com/">在庫管理ページへ</a></div>
-					<div class="box"><a href="http://pla-cole.co/">古本市管理ページへ</a></div>
+					<a class="window_button" href="https://github.com/">在庫管理ページへっ</a>
+					<a class="window_button" href="http://pla-cole.co/">古本市管理ページへっ</a>
 				</div>
 			</div>
             </div>
