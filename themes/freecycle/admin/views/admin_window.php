36f57af09d2d66f2d3f4196e2d743d9d199b17fd
yugogura <jigenbakdaaan@gmail.com>
2015-12-10 23:33:47 +0900

    finish button design itself

--- themes/freecycle/admin/views/admin_window.php
+++ themes/freecycle/admin/views/admin_window.php
@@ -4,8 +4,8 @@
 			</div>
             <div class="wrap">
 				<div class="inner">
-					<a class="window_button" href="https://github.com/">在庫管理ページへっ</a>
-					<a class="window_button" href="http://pla-cole.co/">古本市管理ページへっ</a>
+					<a class="window_button" href="https://github.com/"><span>移動するっぴ！</span><span>在庫管理ページへっ</span></a>
+					<a class="window_button" href="http://pla-cole.co/"><span>移動するっぴ！</span><span>古本市管理ページへっ</span></a>
 				</div>
 			</div>
             </div>
