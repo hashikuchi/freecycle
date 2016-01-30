2e4ede5511d568e96b165e1b8070283a1960febb
hatimitusiropp <0rb3v02778z5y5h@ezweb.ne.jp>
2016-01-30 20:05:10 +0900

    add default bookfair time in textbox

--- themes/freecycle/admin/views/book_fair_page.php
+++ themes/freecycle/admin/views/book_fair_page.php
@@ -4,9 +4,12 @@
     <div id="insert_bookfair_info_area">
         <h3>古本市の予定入力欄</h3>
         <span class="bookfair_label" style="font-size:15px;color:darkorange;">１．開催日、開始時間、終了時間を指定する</span></br>
+        <span>開催日：</span></br>
         <input type="text" placeholder="クリックして開催日を選択" id="bookfair_date" class="bookfair_day"/></br>
-        <input type="text" placeholder="クリックして開始時間を選択" id="start_bookfair_time" class="bookfair_day"/>
-        <input type="text" placeholder="クリックして終了時間を選択" id="end_bookfair_time" class="bookfair_day"/></br>
+        <span>開始時間（12:30以外ならクリックして選択）：</span></br>
+        <input type="text" value="12:30" id="start_bookfair_time" class="bookfair_day"/></br>
+        <span>終了時間（13:30以外ならクリックして選択）：</span></br>
+        <input type="text" value="13:30" id="end_bookfair_time" class="bookfair_day"/></br>
         <span class="bookfair_label" style="font-size:15px;color:darkorange;">２．古本市開催場所を指定する</span></br>
         <select id="bookfair_place">
             <option>南山大学G30</option>
