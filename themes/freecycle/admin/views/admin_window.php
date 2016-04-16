be771a2d8fdbaaf1c2ba3c638e06dadaf8221bcb
yugogura <jigenbakdaaan@gmail.com>
2016-04-17 08:04:49 +0900

    add pages and functions for admin

---
+++ themes/freecycle/admin/views/admin_window.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+		
+<div class="fake">
+        <h1 class="top_char">テクチェ運営<br>トップページ</h1>
+			</div>
+            <div class="wrap">
+				<div class="inner">
+					<a class="window_button" href="https://github.com/"><span>移動するっぴ！</span><span>在庫管理ページへっ</span></a>
+					<a class="window_button" href=" <?php echo home_url(); ?>/book_fair_page"><span>移動するっぴ！</span><span>古本市管理ページへっ</span></a>
+				</div>
+			</div>
+            </div>
+       <!-- <h2>Information</h2>
+            <div class="column_info">
+                <div class="detail1">
+                    <h3>利用者から</h3><a href="http://www.kayac.com/">変えるんだ</a>
+                </div>
+                </div> -->
+            </div>
+    </body>
+</html>
